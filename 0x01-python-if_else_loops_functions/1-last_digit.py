ed913063a959ed0555c8024d98207ac1e98944f9
Prinzwallace <prinzk442@gmail.com>
2022-11-30 17:33:02 -0800

    study to excel

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = str(number)
-num = number[-1]
-if int(num) > 5:
-    print(f'last digit of {number} is {num} and is greater than 5')
-if int(num) < 6 and not int(num) == 0:
-    print(f'last digit of {number} is {num} and is less than 6 and not 0')
-if int(num) == 0:
-    print(f'last digit of {number} is {num} and is 0')
+lastDigit = 0
+
+if number >= 0:
+    lastDigit = number % 10
+else:
+    lastDigit = (-number % 10) * -1
+
+message = f"Last digit of {number} is {lastDigit}"
+
+if lastDigit == 0:
+    print(f"{message} and is 0")
+elif lastDigit > 5 and lastDigit % 10 != 0:
+    print(f"{message} and is greater than 5")
+else:
+    print(f"{message} and is less than 6 and not 0")
